3cc5c1cd2f0f64572c2876d7a72cab461ffbf0b6
Genesis McGruder <genesismcgruder@gmail.com>
2024-05-29 14:59:04 -0400

    done

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,18 @@
 # your code goes here!
+import time
+def countdown(b):
+    while b > 0:
+        print(f'{b} SECOND(S)!')
+        b -= 1
+    else:
+        print("HAPPY NEW YEAR!")
+
+        
+def countdown_with_sleep(b):
+    while b > 0:
+        print(f'{b} SECOND(S)!')
+        time.sleep(1)
+        b -= 1
+    else:
+        print("HAPPY NEW YEAR!")
+
